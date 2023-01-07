7ff66563cef143e7c3bec308c172a182b83c3585
JulesBest176 <119763079+JulesBest176@users.noreply.github.com>
2023-01-06 17:09:42 -0800

    Update Exercice20.c

--- TpStructureConditionnelle/Exercice20/Exercice20.c
+++ TpStructureConditionnelle/Exercice20/Exercice20.c
@@ -2,7 +2,7 @@
 
 int main(){
     int jours, mois, annees, jourAu,moisAu,anneeAu;
-    //une date de naissance doit etre inferieur ou egale à la date actuelle
+    //une date de naissance doit etre inferieur ou egale Ã  la date actuelle
     printf("Veuillez saisir la date d'aujourd'hui aux formats JJ MM AAAA separees par une espace : ");
     scanf("%d %d %d",&jourAu,&moisAu,&anneeAu);
     printf("Veuillez saisir votre date de naissance aux formats JJ MM AAAA separees par une espace : ");
@@ -11,7 +11,7 @@ int main(){
     if (jours<=0 || mois<=0 || annees<0){//ici on verifie si la date saisie ne contient pas des nombres negatifs
         printf("Date Inexistante\n");
     } else {
-        if((annees>anneeAu) || (annees==anneeAu && mois>moisAu) || (annees==anneeAu && mois==moisAu && jours>jourAu)){ //ici on verifie si la date saisie est superieur à la date actuelle
+        if((annees>anneeAu) || (annees==anneeAu && mois>moisAu) || (annees==anneeAu && mois==moisAu && jours>jourAu)){ //ici on verifie si la date saisie est superieur Ã  la date actuelle
             printf("Date Inexistante");
         } else {
             if (jours>31 || mois>12){ // ici on verifie si la date saisit respecte les normes generales en jours et mois
@@ -20,12 +20,16 @@ int main(){
                 if((mois==4 || mois==6 || mois==9 || mois==11) && jours>30){//ici on verifie pour les mois contenant 30jours
                     printf("Date Inexistante");
                 } else {
-                    if(!((annees%4==0 && annees%100!=0)  || (annees%400==0)) && jours>28){//ici on verifie le mois de fevrier dans le cas ou il n'est pas bissextile
+                    if(!((annees%4==0 && annees%100!=0)  || (annees%400==0)) && jours>28  && mois==2){//ici on verifie le mois de fevrier dans le cas ou il n'est pas bissextile
                         printf("Date Inexistante");
                     } else {
-                        printf("Date Existante");
+                         if((annees%4==0 && annees%100!=0)  || (annees%400==0) && jours>29  && mois==2){//ici on verifie le mois de fevrier dans le cas ou il n'est pas bissextile
+                            printf("Date Inexistante");
+                         } else {
+                            printf("Date Existante");
+                         }
                     }
-                }
+             }
             }
         }
     }
